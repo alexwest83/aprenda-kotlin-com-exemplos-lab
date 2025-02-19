12528b6362e0b2a103ccbd58363688af94c38e22
Alexandre Westphal <ads.alexwest@gmail.com>
2025-02-18 21:06:41 -0300

    Update desafio.kt
    Primeiro desafio em Kotlin.

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,66 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
+// [Template no Kotlin Playground](https://play.kotlinlang.org/#eyJ2ZXJzaW9uIjoiMi4xLjEwIiwicGxhdGZvcm0iOiJqYXZhIiwiYXJncyI6IiIsIm5vbmVNYXJrZXJzIjp0cnVlLCJ0aGVtZSI6ImlkZWEiLCJjb2RlIjoidmFsIHNjaG9vbDogU3RyaW5nID0gXCJESU8gLSBEaWdpdGFsIElubm92YXRpb24gT25lXCJcblxudmFsIGxpc3RhQ29udGV1ZG9zRWR1Y2FjaW9uYWlzID0gbXV0YWJsZUxpc3RPZjxDb250ZXVkb0VkdWNhY2lvbmFsPigpXG5cbmVudW0gY2xhc3MgTml2ZWwgeyBCQVNJQ08sIElOVEVSTUVESUFSSU8sIEFWQU5DQURPIH1cblxuZGF0YSBjbGFzcyBVc3VhcmlvKHZhbCBub21lOiBTdHJpbmcpXG5cbmRhdGEgY2xhc3MgQ29udGV1ZG9FZHVjYWNpb25hbCh2YWwgbm9tZTogU3RyaW5nLCB2YWwgZHVyYWNhbzogSW50KSB7XG5cdFxuICAgIGZ1biBhZGljaW9uYXIoKSB7XG4gICAgXHRsaXN0YUNvbnRldWRvc0VkdWNhY2lvbmFpcy5hZGQodGhpcylcbiAgICB9XG59XG5cbmRhdGEgY2xhc3MgRm9ybWFjYW8odmFsIG5vbWU6IFN0cmluZywgdmFsIGNvbnRldWRvczogTGlzdDxDb250ZXVkb0VkdWNhY2lvbmFsPiwgdmFsIG5pdmVsOiBOaXZlbCkge1xuXHRcbiAgICB2YWwgaW5zY3JpdG9zID0gbXV0YWJsZUxpc3RPZjxVc3VhcmlvPigpXG4gICAgXG4gICAgZnVuIG1hdHJpY3VsYXIodXN1YXJpbzogVXN1YXJpbykge1xuICAgICAgICBpbnNjcml0b3MuYWRkKHVzdWFyaW8pXG4gICAgfVxuICAgIFxuICAgIGZ1biBvYnRlckRhZG9zKCl7XG4gICAgICAgIHByaW50bG4oc2Nob29sKVxuICAgICAgICBpbnNjcml0b3MuZm9yRWFjaCgpe1xuICAgICAgICAgICAgdmFyIGR1cmFjYW9Ub3RhbDogSW50ID0gMFxuICAgICAgICBcdHByaW50bG4oKVxuICAgICAgICAgICAgcHJpbnRsbihcIkZvcm1hw6fDo286ICR7bm9tZX1cIilcbiAgICAgICAgICAgIHByaW50bG4oXCJJbmNyaXRvOiAke2l0Lm5vbWV9XCIpXG4gICAgICAgICAgICBwcmludGxuKFwiTml2ZWw6ICR7bml2ZWx9XCIpXG4gICAgICAgICAgICBjb250ZXVkb3MuZm9yRWFjaCgpe1xuICAgICAgICAgICAgICAgIHByaW50bG4oXCJDb250ZcO6ZG86ICR7aXQubm9tZX0uIER1cmHDp8OjbzogJHtpdC5kdXJhY2FvfSBob3Jhcy5cIilcbiAgICAgICAgICAgICAgICBkdXJhY2FvVG90YWwgPSBkdXJhY2FvVG90YWwgKyBpdC5kdXJhY2FvXG4gICAgICAgICAgICB9XG4gICAgICAgICAgICBwcmludGxuKFwiRHVyYcOnw6NvIFRvdGFsOiAkZHVyYWNhb1RvdGFsIGhvcmFzXCIpXG5cdFx0fVxuICAgIH1cbn1cblxuZnVuIG1haW4oKSB7XG4gICAgXG4gICAgLy8gQ3JpYXIgY3Vyc29cbiAgICB2YWwgZm9ybWFjYW8gPSBGb3JtYWNhbyhcIktvdGxpbiBFeHBlcmllbmNlXCIsIGxpc3RhQ29udGV1ZG9zRWR1Y2FjaW9uYWlzLCBOaXZlbC5CQVNJQ08pXG4gICBcdFxuICAgIC8vIEFkaWNpb25hciBjb250ZcO6ZG9cbiAgICBDb250ZXVkb0VkdWNhY2lvbmFsKFwiUHJpbmPDrXBpb3MgZGUgQWdpbGlkYWRlIGUgRGVzZW52b2x2aW1lbnRvIGRlIFNvZnR3YXJlXCIsIDE3KS5hZGljaW9uYXIoKVxuICAgIENvbnRldWRvRWR1Y2FjaW9uYWwoXCJEb21pbmFuZG8gYSBMaW5ndWFnZW0gZGUgUHJvZ3JhbWHDp8OjbyBLb3RsaW5cIiwgMTEpLmFkaWNpb25hcigpXG4gICAgQ29udGV1ZG9FZHVjYWNpb25hbChcIlJlZmluYW5kbyBzdWEgVMOpY25pY2EgY29tIERlc2FmaW9zIGRlIEPDs2RpZ28gZW0gS290bGluXCIsIDUpLmFkaWNpb25hcigpXG4gICAgQ29udGV1ZG9FZHVjYWNpb25hbChcIkZvcnRhbGVjZW5kbyBzZXUgUGVyZmlsIFByb2Zpc3Npb25hbFwiLCA1KS5hZGljaW9uYXIoKVxuICBcdFxuICAgIC8vIEluc2NyZXZlciBhbHVub3NcbiAgICBmb3JtYWNhby5tYXRyaWN1bGFyKFVzdWFyaW8oXCJBbGV4XCIpKVxuICAgIGZvcm1hY2FvLm1hdHJpY3VsYXIoVXN1YXJpbyhcIkpvw6NvXCIpKVxuICAgIGZvcm1hY2FvLm1hdHJpY3VsYXIoVXN1YXJpbyhcIkZlbGlwZVwiKSlcbiAgICBmb3JtYWNhby5tYXRyaWN1bGFyKFVzdWFyaW8oXCJSYWZhZWxcIikpXG4gICAgZm9ybWFjYW8ubWF0cmljdWxhcihVc3VhcmlvKFwiUGF1bG9cIikpXG4gICAgZm9ybWFjYW8ubWF0cmljdWxhcihVc3VhcmlvKFwiTWFyY2Vsb1wiKSlcbiAgICBmb3JtYWNhby5tYXRyaWN1bGFyKFVzdWFyaW8oXCJWZW5pbHRvblwiKSlcbiAgICBmb3JtYWNhby5tYXRyaWN1bGFyKFVzdWFyaW8oXCJBbGV4YW5kcmVcIikpXG4gICAgXG4gICAgLy8gSW1wcmltaXJcbiAgICBmb3JtYWNhby5vYnRlckRhZG9zKClcbn0ifQ==)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+val school: String = "DIO - Digital Innovation One"
 
-class Usuario
+val listaConteudosEducacionais = mutableListOf<ConteudoEducacional>()
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Usuario(val nome: String)
 
+data class ConteudoEducacional(val nome: String, val duracao: Int) {
+	
+    fun adicionar() {
+    	listaConteudosEducacionais.add(this)
+    }
+}
+
+data class Formacao(val nome: String, val conteudos: List<ConteudoEducacional>, val nivel: Nivel) {
+	
     val inscritos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
+    }
+    
+    fun obterDados(){
+        println(school)
+        inscritos.forEach(){
+            var duracaoTotal: Int = 0
+        	println()
+            println("Formação: ${nome}")
+            println("Incrito: ${it.nome}")
+            println("Nivel: ${nivel}")
+            conteudos.forEach(){
+                println("Conteúdo: ${it.nome}. Duração: ${it.duracao} horas.")
+                duracaoTotal = duracaoTotal + it.duracao
+            }
+            println("Duração Total: $duracaoTotal horas")
+		}
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    
+    // Criar curso
+    val formacao = Formacao("Kotlin Experience", listaConteudosEducacionais, Nivel.BASICO)
+   	
+    // Adicionar conteúdo
+    ConteudoEducacional("Princípios de Agilidade e Desenvolvimento de Software", 17).adicionar()
+    ConteudoEducacional("Dominando a Linguagem de Programação Kotlin", 11).adicionar()
+    ConteudoEducacional("Refinando sua Técnica com Desafios de Código em Kotlin", 5).adicionar()
+    ConteudoEducacional("Fortalecendo seu Perfil Profissional", 5).adicionar()
+  	
+    // Inscrever alunos
+    formacao.matricular(Usuario("Alex"))
+    formacao.matricular(Usuario("João"))
+    formacao.matricular(Usuario("Felipe"))
+    formacao.matricular(Usuario("Rafael"))
+    formacao.matricular(Usuario("Paulo"))
+    formacao.matricular(Usuario("Marcelo"))
+    formacao.matricular(Usuario("Venilton"))
+    formacao.matricular(Usuario("Alexandre"))
+    
+    // Imprimir
+    formacao.obterDados()
 }
